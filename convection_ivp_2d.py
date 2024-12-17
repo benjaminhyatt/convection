b67d6367ea3996fd64a614ba8ac37da2b8134d01
benjaminhyatt <ben@Bens-MacBook-Pro-9.local>
2024-12-17 10:29:07 -0500

    adding first few papers, and beginning of first ivp script

---
+++ convection_ivp_2d.py
@@ -0,0 +1,30 @@
+"""
+Dedalus (d3) script to run a 2d horizontally-periodic simulation 
+with convection and stable stratification in the Boussinesq limit.
+
+Usage:
+"""
+import numpy as np
+import dedalus.public as d3
+import logging
+logger = logging.getLogger(__name__)
+
+### Parameters ###
+
+### Bases ###
+
+### Fields ###
+
+### Substitutions ###
+
+### Problem ###
+
+### Solver ###
+
+### Initial conditions ###
+
+### Analysis tasks ###
+
+### Flow tools ###
+
+### Main loop ###
